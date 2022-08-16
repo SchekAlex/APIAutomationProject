92b1ba5b633f0067ebb60da4bb988576c6a7f944
SchekAlex <schekalexandru.iulian@gmail.com>
2022-08-16 20:45:08 +0300

    First Push

---
+++ src/test/java/com/petstore/user/TestBase.java
@@ -0,0 +1,59 @@
+package com.petstore.user;
+
+import com.petstore.user.userservice.UserService;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class TestBase {
+    protected static final UserService userApiService = UserService.getInstance();
+
+    /* For making requests */
+    public static String userServiceBaseUrl;
+
+    /* Taking values from the properties file */
+    private void readAppProperties(){
+        FileInputStream fis = null;
+        Properties testProperties = null;
+
+        try{
+            fis = new FileInputStream("/src/test/resources/configs/user/app.properties");
+            testProperties = new Properties();
+            testProperties.load(fis);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        userServiceBaseUrl = testProperties.getProperty("baseUrl");
+
+        try{
+            if(fis!=null){
+                fis.close();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void readTestProperties(){
+        FileInputStream fis = null;
+        Properties testProperties = null;
+
+        try{
+            fis = new FileInputStream("/src/test/resources/configs/user/test.properties");
+            testProperties = new Properties();
+            testProperties.load(fis);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        try{
+            if(fis!=null){
+                fis.close();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/com/petstore/user/userservice/UserService.java
@@ -0,0 +1,57 @@
+package com.petstore.user.userservice;
+
+import com.petstore.user.TestBase;
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import static io.restassured.RestAssured.given;
+
+
+public class UserService {
+    private static final UserService userServiceInstance = new UserService();
+    private static final Logger logger = LogManager.getLogger();
+    private UserService(){}
+
+    public Response postCreateWithArray(){
+        RequestSpecification requestSpecification = RestAssured.given();
+        requestSpecification.contentType(ContentType.JSON);
+
+        return userApiRequest("user/createWithArray", Method.POST, requestSpecification)
+                .then().extract().response();
+    }
+    public static UserService getInstance(){return userServiceInstance;}
+
+
+    /**
+     * Main method for making requests with Rest Assured to "UserServiceApi" User Calls
+     * @param methodName - the name of the method used for the request(Ex: /user/createWithArray)
+     * @param httpMethod  - the type of the HTTP method used for the request (Ex: GET/POST)*
+     * @param requestSpecification additional Request Specification to be used in the request (Ex: body data)
+     * @return
+     */
+    private Response userApiRequest(String methodName, Method httpMethod, RequestSpecification requestSpecification){
+        final String userServiceApiBaseUrl = TestBase.userServiceBaseUrl;
+        logger.info("Request Base URL: "+ httpMethod + " " + userServiceApiBaseUrl + "/" + methodName);
+
+        requestSpecification.header("X-TrackerContext-Caller","UserServiceApiTesting");
+        requestSpecification.header("X-TrackerContext-CallerIdentifier", "ApiTesting-UserService");
+
+        return given()
+                .filter(new AllureRestAssured())
+                .filter(new RequestLoggingFilter(System.out))
+                .filter(new ResponseLoggingFilter(System.out))
+                .spec(requestSpecification)
+                .baseUri(userServiceApiBaseUrl)
+                .when().request(httpMethod,methodName)
+                .then().extract().response();
+
+    }
+}
